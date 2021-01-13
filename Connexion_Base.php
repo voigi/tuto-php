19851fd5d42a1cff51e0db79a5d3e918f0576a80
voigi <supervoigi@gmail.com>
2021-01-13 18:07:26 +0100

    Ajout methode  des marqueurs

--- Connexion_Base.php
+++ Connexion_Base.php
@@ -1,13 +1,10 @@
 
     <?php
     //connexion à la base de données jeux_video//
-    try
-    {
+    try {
         $bdd = new PDO('mysql:host=localhost;dbname=jeux_video;charset=utf8', 'root', '');
-       // echo 'Connexion établie';
-    }
-    catch(Exception $e)
-    {
+        // echo 'Connexion établie';
+    } catch (Exception $e) {
         die('Erreur de connexion');
     }
 
@@ -18,25 +15,37 @@
     $reponse = $bdd->query('SELECT * FROM jeux_video');
 
     //On parcourt le contenu de la base//
-    while($donnees = $reponse->fetch())
-    {
-        echo '<b>'.'Jeu'.'</b>'.':'.$donnees['nom'].'<br>';
-        echo 'Le posseseur de ce jeu est :'.$donnees['possesseur'].','.'et il le vend à '.$donnees['prix'].' euros !'.'<br>';
-        echo  'Ce jeu fonctionne sur '.$donnees['console'].' et on peut y jouer à '.$donnees['nbre_joueurs_max'].' maximum'.'<br>';
-        echo  $donnees['possesseur'].' a laissé ces commentaires sur '.$donnees['nom'].' : '.'<i>'.$donnees['commentaires'].'</i><br><br>';
-    } 
+    while ($donnees = $reponse->fetch()) {
+        echo '<b>' . 'Jeu' . '</b>' . ':' . $donnees['nom'] . '<br>';
+        echo 'Le posseseur de ce jeu est :' . $donnees['possesseur'] . ',' . 'et il le vend à ' . $donnees['prix'] . ' euros !' . '<br>';
+        echo  'Ce jeu fonctionne sur ' . $donnees['console'] . ' et on peut y jouer à ' . $donnees['nbre_joueurs_max'] . ' maximum' . '<br>';
+        echo  $donnees['possesseur'] . ' a laissé ces commentaires sur ' . $donnees['nom'] . ' : ' . '<i>' . $donnees['commentaires'] . '</i><br><br>';
+    }
 
     $reponse->closeCursor();
-    
+
     //Affichons les jeux de Patrick avec une requête preparé//
 
     $req = $bdd->prepare('SELECT nom FROM jeux_video WHERE possesseur = ?');
     $req->execute(array('Patrick'));
 
-    while($donnee = $req->fetch())
-    {
-       echo 'Patrick possède '.$donnee['nom'].'<br>';
-    } 
-    
-?>
+    while ($donnee = $req->fetch()) {
+        echo 'Patrick possède ' . $donnee['nom'] . '<br>';
+    }
+
+    //Affichons les jeux de Michel avec la méthode des Marqueurs //
+
+    $reponse = $bdd->prepare(' SELECT nom FROM jeux_video WHERE possesseur = ?'); // le ? c quand l'utilisateur saisi une données
+    $reponse->execute(array('Michel'));
+
+    while ($donnees = $reponse->fetch()) {
+        echo " les jeux posséder par michel sont : " . $donnees['nom'] . "<br><br><br>";
+    }
+
+    $reponse = $bdd->prepare(' SELECT * FROM jeux_video WHERE possesseur = :possesseur AND prix <= :prixmax ORDER BY prix');
+    $reponse->execute(array('possesseur' => 'Patrick', 'prixmax' => 25));
+    while ($donnees = $reponse->fetch()) {
+        echo " les jeux posséder par michel sont : " . $donnees['nom'] . " " . $donnees['prix'] . " €<br>";
+    }
+    ?>
 
