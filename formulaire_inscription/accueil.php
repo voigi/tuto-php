2d5b8969665b538e4f39548b5e96605c956534b8
voigi <supervoigi@gmail.com>
2021-01-20 17:47:06 +0100

    exo = debut tp 3

--- formulaire_inscription/accueil.php
+++ formulaire_inscription/accueil.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
     <h3>Bienvenue
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="accueil.php">Acceuil</a></li>
+                    <li><a href="profil.php">Profil</a></li>
+                    <li><a href="connect.php">Connexion</a></li>
+                </ul>
+            </nav>
+            <img src="formulaire_inscription\images\téléchargement.jpg" alt="image">
+            <h1></h1>
+            <p></p>
+            <p></p>
+            <p></p>
+        </header>
         <?php
             session_start();
         ?>
--- formulaire_inscription/profil.php
+++ formulaire_inscription/profil.php
@@ -4,13 +4,29 @@
 <?php
 include('Connexion.php');
 include('bootstrap.html');
-echo 'Bonjour '. $_SESSION ['pseudo'].'<br>';
+echo 'Bonjour '. @$_SESSION ['pseudo'].'<br><br>';
 
 if(isset($_POST ['deco'])){
    session_destroy();
    header('Location:accueil.php');
 }
  ?>
+ <?php
+  if(isset($_FILES['photo']) && $_FILES['photo']['error'] == 0)
+  {
+    $photoname=$_FILES['photo']['name'];
+    $phototype=$_FILES['photo']['type'];
+    $photosize=$_FILES['photo']['size'];
+
+    move_uploaded_file($_FILES['photo']['tmp_name'],"images/".$_FILES['photo']['name']);
+  }
+ ?>
+ <form method="post" enctype="multipart/form-data">
+   <label for="upload">Fichier:</label>
+   <input type="file" name="photo" id="upload">
+   <input type="submit" name="submit" value="Upload"> 
+ </form>
+ <br>
  <form  method="POST">
      <input type="submit" id="deco" name="deco"  value="Deconnexion" class="btn btn-primary">
  </form>
