24d32f15f082c87728ef64bf5e8454e04bee8362
voigi <supervoigi@gmail.com>
2021-02-16 20:44:17 +0100

    modif profil

--- formulaire_inscription/profil.php
+++ formulaire_inscription/profil.php
@@ -8,16 +8,20 @@ include('Connexion.php');
 include('bootstrap.html');
 echo 'Bonjour '. $_SESSION ['pseudo'].'<br>';
 
+$deconnexion = $_POST['deconnexion'];
+var_dump($deconnexion);
+if (isset($deconnexion)){
+    header("Location: accueil.html");
+}
+
+else{
+    echo "erreur";
+}
  ?>
  
- <button class="btn btn-primary" id="deco"> Deconnexion</button>
-
- <script>
-    var btn = document.getElementById('deco');
-    btn.addEventListener('click', function() {
-      document.location.href = 'formulaire_inscription.html';
-    });
-  </script>
+ <form action="profil.php">
+  <input type="submit" value="deconnexion">
+ </form>
 
 
 
